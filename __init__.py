356840131234acb64e3556a45542b75100b40e8a
root <naxaal@gmail.com>
2017-08-14 11:32:31 +0500

    now needs to reverse post order, shorten date and include paginations

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
-from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Sequence, CreateSequence
+from datetime import datetime
+from math import ceil
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///mnazaal'
@@ -27,17 +29,18 @@ db = SQLAlchemy(app)
 class Logmessage(db.Model):
 	__tablename__ = "logmessages"
 	id = db.Column(db.Integer, primary_key=True)
+	datetime = db.Column(db.DateTime)
 	message = db.Column(db.String(200)) #message posted
-	user_name = db.Column(db.String(80) )#, ForeignKey("logappuser.username")) #the user who posted the message
+	user_name = db.Column(db.String(80))#, ForeignKey("logappuser.username")) #the user who posted the message
 
-	def __init__(self, id, message, username):
-		self.id = id
+	def __init__(self, datetime, message, username):
+		self.datetime = datetime
 		self.message = message
-		self.username = username
+		self.user_name = username
 
-	def __repr__(self, message):
-		return '<Username %r' % self.username  #check this line
-		return '<Message %r' % self.message #check this line
+	# def __repr__(self, message):
+	# 	return '<Username %r' % self.username  #check this line
+	# 	return '<Message %r' % self.message #check this line
 
 
 db.create_all() #creating the databases
@@ -45,17 +48,22 @@ db.create_all() #creating the databases
 
 @app.route("/")
 def homepage():
-	return render_template("index.html")
+	now = datetime.now()
+	return render_template("index.html", messagelist=Logmessage.query.all()) #gets database items as an attribute
 
 @app.route("/", methods = ['POST'])
 def message_post():
 	message = None
+	now = datetime.now()
 	if request.method == "POST":
-		text = request.form["messagebox"]  #enter the name attribute of form element in []
-		msg = Logmessage(101,text,'admin') #needs to generate a unique primary key
-		db.session.add(msg)
+		msgtext = request.form["messagebox"]  #enter the name attribute of form element within []
+		data = Logmessage(datetime.utcnow(), str(msgtext), "nazaal") #must change username, taking it from external server
+		db.session.add(data)
 		db.session.commit()
-		return render_template("index.html")
+		return render_template("index.html", messagelist=Logmessage.query.all())
+
+
+
 
 
 
