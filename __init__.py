60a25d4919e03b99da741cd304f08c51d5758ba7
root <naxaal@gmail.com>
2017-08-15 08:11:56 +0500

    posts now show in descending order

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime
+from sqlalchemy import Table, Column, Integer, ForeignKey, DateTime, desc
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Sequence, CreateSequence
@@ -27,7 +27,7 @@ db = SQLAlchemy(app)
 # 		return '<Message %r' % self.message #check this line
 
 class Logmessage(db.Model):
-	__tablename__ = "logmessages"
+	__tablename__ = "logmessages"  #table name in postgres
 	id = db.Column(db.Integer, primary_key=True)
 	datetime = db.Column(db.DateTime)
 	message = db.Column(db.String(200)) #message posted
@@ -49,7 +49,7 @@ db.create_all() #creating the databases
 @app.route("/")
 def homepage():
 	now = datetime.now()
-	return render_template("index.html", messagelist=Logmessage.query.all()) #gets database items as an attribute
+	return render_template("index.html", messagelist=Logmessage.query.order_by(desc(Logmessage.datetime)).all()) #gets database items as an attribute
 
 @app.route("/", methods = ['POST'])
 def message_post():
@@ -60,8 +60,8 @@ def message_post():
 		data = Logmessage(datetime.utcnow(), str(msgtext), "nazaal") #must change username, taking it from external server
 		db.session.add(data)
 		db.session.commit()
-		return render_template("index.html", messagelist=Logmessage.query.all())
-	return render_template("index.html", messagelist=Logmessage.query.all())
+		return render_template("index.html", messagelist=Logmessage.query.order_by(desc(Logmessage.datetime)).all())
+	return render_template("index.html", messagelist=Logmessage.query.order_by(desc(Logmessage.datetime)).all())
 
 @app.route('/<int:page>',methods=['GET'])
 def view(page=1):
