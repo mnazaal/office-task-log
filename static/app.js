ad1309e5acec449b9e1fb47228f9a528029ae56f
root <naxaal@gmail.com>
2017-09-13 16:15:09 +0500

    finishing for wednesday, need to fix post api to receive json data to python db

--- static/app.js
+++ static/app.js
@@ -3,20 +3,40 @@ console.log("/var/www/logapp/static/app.js");
 var messageBoardApp = angular.module("messageBoardApp", ['angular.filter']);  //defining a module named messageBoard
 //reminder to install angular-filter via npm or bower
 
-messageBoardApp.controller("MessageController", function($scope, $http){  //registering the controller with the app/module, using the controller method with the controller and a constructor method as arguements, $scope object refers to current context for the view/it is the model used by the view
+messageBoardApp.controller("ShowController", function($scope, $http){  //registering the controller with the app/module, using the controller method with the controller and a constructor method as arguements, $scope object refers to current context for the view/it is the model used by the view
 	var searchbutton = document.getElementById("date");
 	console.log(searchbutton)
 
-	$http.get("http://127.0.0.2:5000/messages")
+	$http.get("http://127.0.0.1/api/messages")
 		.then(function(response) {
 			$scope.items = response.data;
-			console.log("success whole list")  //debugging code
+			console.log("Received data successfully")  //debugging code
 		},
 		function(error) {
 			console.log(error)
 		});
 });
 
+messageBoardApp.controller("PostController", function($scope, $http){
+	$scope.postdata = function(message){
+		var date = new Date();  //getting current date
+		$scope.datestring = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+		var data = {
+			date: $scope.datestring,
+			username: "nazaal", //also gonna fix soon
+			message: message,
+			time: "right now"  //gonna fix soon
+		}
+		$http.post('api/messages', JSON.stringify(data))
+			.then(function(response) {
+				if (response.data)
+				console.log("Post data send successfully");
+			},
+			function(error) {
+				console.log(error)
+			});
+	}
+})
 //routeparams
 
 // messageBoardApp.controller("SearchController", function($scope, $http){
